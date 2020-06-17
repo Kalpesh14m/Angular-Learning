82984e9cc47375ab9082d7009420c73bd0fbdf0b
kalpesh14m <kalpesh.1.mali@gmail.com>
2020-06-18 02:12:13 +0530

    added second app

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "my-app-main",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
+})
+
+export class AppComponent{
+    name : string = "Kalpesh";
+
+}
+
